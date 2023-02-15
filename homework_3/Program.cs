244728fca0a7acc8ae3827d8c149509e82943e84
Katerina Mavlikhanova <madam.andreewna2016@yandex.ru>
2023-02-15 23:47:44 +0300

    add new info to homework_3

---
+++ homework_3/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] array = new int[1];
+
+void InitArray(int[] array)
+{
+int length = array.Length; 
+int index = 0;
+Random rnd = new Random(); 
+while (index < length) 
+{
+array[index] = rnd.Next(); 
+}
+}
+
+void PrintArray(int [] array)
+{
+for (int i = 0; i < array.Length; i++ )
+{
+Console.WriteLine(array[i]);
+}
+
+}
+
+InitArray(array);
+PrintArray(array);
