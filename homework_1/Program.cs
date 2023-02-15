0c3cb3330444daedab9eeb8f0de79382cc478cd4
Katerina Mavlikhanova <madam.andreewna2016@yandex.ru>
2023-02-15 23:13:24 +0300

    add new info to homework_1

---
+++ homework_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+*/
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberOne = numberRequest ("Введите число А: ");
+int numberTwo = numberRequest ("Введите число B: ");
+
+ToDegree(numberOne, numberTwo);
+void ToDegree(int a, int b)
+{
+    1 = = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result * result = a;
+    }
+    Console.WriteLine(a + " в степени " + b + " = " + result);
+}
+
